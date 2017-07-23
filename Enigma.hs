335019e1c26f1ea8e5ad15c08ae8cf1d736fe21e
Václav Klikar <30387059+vklikar@users.noreply.github.com>
2017-07-23 14:55:40 +0200

    Add the first version of the Enigma

---
+++ Enigma.hs
@@ -0,0 +1,115 @@
+-- usage:
+--     encrypt (Key [_I, _II, _III] "AAA") "PLAINTEXT"
+
+import Data.Map (Map, fromList, toList, (!))
+import Data.Tuple (swap)
+import Data.Char (ord)
+import Debug.Trace
+
+data Key = Key {
+    wheelOrder :: [Wheel],
+    --ringSetting :: String,
+    basicWheelSetting :: String
+    --crossPlugging :: [String]
+} deriving (Show)
+
+data Wheel = Wheel {
+    wiring :: Map Char Char,
+    notch :: String,
+    turnover :: String,
+    startPosition :: Char
+} deriving (Show)
+
+data Enigma = Enigma {
+    entryWheel :: Wheel,
+    leftWheel :: Wheel,
+    middleWheel :: Wheel,
+    rightWheel :: Wheel,
+    reflector :: Wheel
+} deriving (Show)
+
+-- Enigma M3
+-- http://www.cryptomuseum.com/crypto/enigma/wiring.htm#4
+--------------------------------------------------------------
+-- Wheel            ABCDEFGHIJKLMNOPQRSTUVWXYZ  Notch Turnover
+--------------------------------------------------------------
+_ETW  =  makeWheel "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ""   ""
+_I    =  makeWheel "EKMFLGDQVZNTOWYHXUSPAIBRCJ" "Y"  "Q"
+_II   =  makeWheel "AJDKSIRUXBLHWTMCQGZNPYFVOE" "M"  "E"
+_III  =  makeWheel "BDFHJLCPRTXVZNYEIWGAKMUSQO" "D"  "V"
+_IV   =  makeWheel "ESOVPZJAYQUIRHXLNFTGKDCMWB" "R"  "J"
+_V    =  makeWheel "VZBRGITYUPSDNHLXAWMJQOFECK" "H"  "Z"
+_VI   =  makeWheel "JPGVOUMFYQBENHZRDKASXLICTW" "HU" "ZM"
+_VII  =  makeWheel "NZJHGRCXMYSWBOUFAIVLPEKQDT" "HU" "ZM"
+_VIII =  makeWheel "FKQHTLXOCBJSPDZRAMEWNIUYGV" "HU" "ZM"
+_UKWB =  makeWheel "YRUHQSLDPXNGOKMIEBFZCWVJAT" ""   ""
+_UKWC =  makeWheel "FVPJIAOYEDRZXWGCTKUQSBNMHL" ""   ""
+
+pad x = replicate (4 - length x) ' ' ++ x
+show' x = pad $ show x
+
+letters :: String
+letters = ['A'..'Z']
+
+lettersCount :: Int
+lettersCount = 26
+
+index :: Char -> Int
+index c = ord c - ord 'A'
+
+revIndex :: Char -> Int
+revIndex c = ord 'Z' - ord c
+
+makeWiring :: String -> Map Char Char
+makeWiring = fromList . zip ['A'..'Z']
+
+makeWheel :: String -> String -> String -> Wheel
+makeWheel wiring@(startPosition:_) notch turnover = Wheel (makeWiring wiring) notch turnover startPosition
+
+setWheel :: Wheel -> Char -> Wheel
+setWheel wheel startPosition = wheel { startPosition = startPosition }
+
+inv :: Wheel -> Wheel
+inv wheel = wheel { wiring = fromList . map swap $ toList $ wiring wheel }
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = take (length xs) $ drop (n `mod` length xs) $ cycle xs
+
+translate :: Int -> Char -> Char
+translate n = (!) $ makeWiring (rotate n ['A'..'Z'])
+
+transform ::  Wheel -> Char -> Char
+transform wheel = (!) (wiring wheel)
+
+encrypt':: Enigma -> Int -> String -> String
+encrypt' _ _ [] = []
+encrypt' enigma@(Enigma etw l m r ukw) n (x:xs) =
+    -- trace ("n: " ++ (show' n) ++ ", start: " ++ (show' $ index $ startPosition r) ++ ", mod: " ++ (show' $ i `mod` 26) ++ ",       i: " ++ (show' i) ++ " (" ++ show (letters !! (i `mod` 26)) ++ "),       j: " ++ (show' j) ++ " (" ++ show (letters !! (j `mod` 26)) ++ "),       k: " ++ (show' k) ++ " (" ++ show (letters !! (k `mod` 26)) ++ ")")$
+    ( translate (-i) . transform (inv r) . translate i
+    . translate (-j) . transform (inv m) . translate j
+    . translate (-k) . transform (inv l) . translate k
+    . transform ukw
+    . translate (-k) . transform l . translate k
+    . translate (-j) . transform m . translate j
+    . translate (-i) . transform r . translate i) x : encrypt' enigma (n + 1) xs
+    where
+        i = startIndex r + n
+        j = startIndex m + turnoverCount 1 i r
+        j' = startIndex m + turnoverCount 2 i r
+        k = startIndex l + turnoverCount 0 j' m
+        turnoverCount offset ind wheel =
+            sum [(ind - offset + (revIndex t)) `div` lettersCount - (if startIndex wheel > index t then 1 else 0) | t <- turnover wheel]
+        startIndex = index . startPosition
+
+encrypt :: Key -> String -> String
+encrypt (Key (wheelL:wheelM:wheelR:_) (startL:startM:startR:_)) = encrypt' (Enigma etw l m r ukw) 1
+    where
+        etw = setWheel _ETW 'A'
+        l = setWheel wheelL startL
+        m = setWheel wheelM startM
+        r = setWheel wheelR startR
+        ukw = setWheel _UKWB 'A'
+
+
+--  encrypt (Key [_I, _II, _III] "CFA") (take 100 $ repeat 'A')
+--  SQPEUGJRCXZWPFYIYYBWLJEWROUVKPOZTCEUWTFJZQWPBQLOTTSRMDFLGXBXZRYQKDGJRZEZMDHJNQYPDJWCJFJLFNTRSNCNLGSK
