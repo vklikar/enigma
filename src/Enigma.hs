2583810deffc23deaa88ce01736d3be9fe39883b
Václav Klikar <30387059+vklikar@users.noreply.github.com>
2019-06-24 22:43:18 +0200

    Make functions more testable and add unit tests

--- src/Enigma.hs
+++ src/Enigma.hs
@@ -4,7 +4,8 @@ module Enigma where
 
 import Data.Map (Map, fromList, toList, (!), union)
 import Data.Tuple (swap)
-import Data.Char (ord)
+import Data.Maybe (fromJust)
+import Data.List (elemIndex, reverse)
 import Debug.Trace
 
 data Key = Key {
@@ -20,9 +21,10 @@ data Wheel = Wheel {
     turnover :: String,
     startPosition :: Char,
     ringOffset :: Char
-} deriving (Show)
+} deriving (Eq, Show)
 
 data Enigma = Enigma {
+    alphabet :: String,
     plugboard :: Map Char Char,
     entryWheel :: Wheel,
     leftWheel :: Wheel,
@@ -33,44 +35,44 @@ data Enigma = Enigma {
 
 -- Enigma M3
 -- http://www.cryptomuseum.com/crypto/enigma/wiring.htm#4
---------------------------------------------------------------
--- Wheel            ABCDEFGHIJKLMNOPQRSTUVWXYZ  Notch Turnover
---------------------------------------------------------------
-_ETW  =  makeWheel "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ""   ""
-_I    =  makeWheel "EKMFLGDQVZNTOWYHXUSPAIBRCJ" "Y"  "Q"
-_II   =  makeWheel "AJDKSIRUXBLHWTMCQGZNPYFVOE" "M"  "E"
-_III  =  makeWheel "BDFHJLCPRTXVZNYEIWGAKMUSQO" "D"  "V"
-_IV   =  makeWheel "ESOVPZJAYQUIRHXLNFTGKDCMWB" "R"  "J"
-_V    =  makeWheel "VZBRGITYUPSDNHLXAWMJQOFECK" "H"  "Z"
-_VI   =  makeWheel "JPGVOUMFYQBENHZRDKASXLICTW" "HU" "ZM"
-_VII  =  makeWheel "NZJHGRCXMYSWBOUFAIVLPEKQDT" "HU" "ZM"
-_VIII =  makeWheel "FKQHTLXOCBJSPDZRAMEWNIUYGV" "HU" "ZM"
-_UKWB =  makeWheel "YRUHQSLDPXNGOKMIEBFZCWVJAT" ""   ""
-_UKWC =  makeWheel "FVPJIAOYEDRZXWGCTKUQSBNMHL" ""   ""
+-------------------------------------------------------------------------
+-- Alphabet
+-------------------------------------------------------------------------
+_AZ   =  ['A'..'Z']
+
+------------------------------------------------------------------
+-- Wheel                ABCDEFGHIJKLMNOPQRSTUVWXYZ  Notch Turnover
+------------------------------------------------------------------
+_ETW  =  makeWheel _AZ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ""   ""
+_I    =  makeWheel _AZ "EKMFLGDQVZNTOWYHXUSPAIBRCJ" "Y"  "Q"
+_II   =  makeWheel _AZ "AJDKSIRUXBLHWTMCQGZNPYFVOE" "M"  "E"
+_III  =  makeWheel _AZ "BDFHJLCPRTXVZNYEIWGAKMUSQO" "D"  "V"
+_IV   =  makeWheel _AZ "ESOVPZJAYQUIRHXLNFTGKDCMWB" "R"  "J"
+_V    =  makeWheel _AZ "VZBRGITYUPSDNHLXAWMJQOFECK" "H"  "Z"
+_VI   =  makeWheel _AZ "JPGVOUMFYQBENHZRDKASXLICTW" "HU" "ZM"
+_VII  =  makeWheel _AZ "NZJHGRCXMYSWBOUFAIVLPEKQDT" "HU" "ZM"
+_VIII =  makeWheel _AZ "FKQHTLXOCBJSPDZRAMEWNIUYGV" "HU" "ZM"
+_UKWB =  makeWheel _AZ "YRUHQSLDPXNGOKMIEBFZCWVJAT" ""   ""
+_UKWC =  makeWheel _AZ "FVPJIAOYEDRZXWGCTKUQSBNMHL" ""   ""
 
 pad x = replicate (4 - length x) ' ' ++ x
-show' x = pad $ show x
+showPadded x = pad $ show x
 
-letters :: String
-letters = ['A'..'Z']
+index :: String -> Char -> Int
+index az c = fromJust $ elemIndex c az
 
-lettersCount :: Int
-lettersCount = 26
+revIndex :: String -> Char -> Int
+revIndex = index . reverse
 
-index :: Char -> Int
-index c = ord c - ord 'A'
+makeWiring :: String -> String -> Map Char Char
+makeWiring az = fromList . zip az
 
-revIndex :: Char -> Int
-revIndex c = ord 'Z' - ord c
+makeWheel :: String -> String -> String -> String -> Wheel
+makeWheel az wiring@(startPosition:_) notch turnover =
+    Wheel (makeWiring az wiring) notch turnover startPosition 'A'
 
-makeWiring :: String -> Map Char Char
-makeWiring = fromList . zip ['A'..'Z']
-
-makeWheel :: String -> String -> String -> Wheel
-makeWheel wiring@(startPosition:_) notch turnover = Wheel (makeWiring wiring) notch turnover startPosition 'A'
-
-makePlugboard :: [String] -> Map Char Char
-makePlugboard xs = (fromList pairs) `union` (fromList $ map swap pairs) `union` makeWiring letters
+makePlugboard :: String -> [String] -> Map Char Char
+makePlugboard az xs = fromList pairs `union` fromList (map swap pairs) `union` makeWiring az az
     where pairs = [(x, y) | (x:y:_) <- xs]
 
 setWheel :: Wheel -> Char -> Char -> Wheel
@@ -85,46 +87,84 @@ inv' = fromList . map swap . toList
 rotate :: Int -> [a] -> [a]
 rotate n xs = take (length xs) $ drop (n `mod` length xs) $ cycle xs
 
-translate :: Int -> Char -> Char
-translate n = (!) $ makeWiring (rotate n ['A'..'Z'])
+translate :: String -> Int -> Char -> Char
+translate az n = (!) $ makeWiring az (rotate n az)
 
 transform ::  Wheel -> Char -> Char
 transform wheel = (!) (wiring wheel)
 
 encrypt':: Enigma -> Int -> String -> String
 encrypt' _ _ [] = []
-encrypt' enigma@(Enigma p etw l m r ukw) n (x:xs) =
-    -- trace ("n: " ++ (show' n) ++ ", start: " ++ (show' $ index $ startPosition r) ++ ", mod: " ++ (show' $ i `mod` 26) ++ ",       i: " ++ (show' i) ++ " (" ++ show (letters !! (i `mod` 26)) ++ "),       j: " ++ (show' j) ++ " (" ++ show (letters !! (j `mod` 26)) ++ "),       k: " ++ (show' k) ++ " (" ++ show (letters !! (k `mod` 26)) ++ ")")$
+encrypt' enigma@(Enigma az p etw l m r ukw) n (x:xs) =
+    -- trace (show steps
+    --     ++ ", n: " ++ showPadded n
+    --     ++ ", mod: " ++ showPadded iMod
+    --     ++ ", i: " ++ showPadded i ++ " (" ++ show iChar ++ ")"
+    --     ++ ", j: " ++ showPadded j ++ " (" ++ show jChar ++ ")"
+    --     ++ ", k: " ++ showPadded k ++ " (" ++ show kChar ++ ")")
     ( (!) p
-    . translate (-i) . transform (inv r) . translate i
-    . translate (-j) . transform (inv m) . translate j
-    . translate (-k) . transform (inv l) . translate k
+    . translate az (-i) . transform (inv r) . translate az i
+    . translate az (-j) . transform (inv m) . translate az j
+    . translate az (-k) . transform (inv l) . translate az k
     . transform ukw
-    . translate (-k) . transform l . translate k
-    . translate (-j) . transform m . translate j
-    . translate (-i) . transform r . translate i
+    . translate az (-k) . transform l . translate az k
+    . translate az (-j) . transform m . translate az j
+    . translate az (-i) . transform r . translate az i
     . (!) (inv' p)) x : encrypt' enigma (n + 1) xs
     where
+        -- -------------------------------------------------------------------
+        -- TODO: Delete this later, used only for debugging.
+        -- -------------------------------------------------------------------
+        -- azLength = length az
+        -- iMod = i `mod` azLength
+        -- jMod = j `mod` azLength
+        -- kMod = k `mod` azLength
+        -- iChar = az !! iMod
+        -- jChar = az !! jMod
+        -- kChar = az !! kMod
+        -- p1 = (!) (inv' p) x
+        -- r1 = translate az i p1
+        -- r2 = transform r r1
+        -- r3 = translate az (-i) r2
+        -- m1 = translate az j r3
+        -- m2 = transform m m1
+        -- m3 = translate az (-j) m2
+        -- l1 = translate az k m3
+        -- l2 = transform m l1
+        -- l3 = translate az (-k) l2
+        -- u1 = transform ukw l3
+        -- l4 = translate az k u1
+        -- l5 = transform (inv l) l4
+        -- l6 = translate az (-k) l5
+        -- m4 = translate az j l6
+        -- m5 = transform (inv m) m4
+        -- m6 = translate az (-j) m5
+        -- r4 = translate az i m6
+        -- r5 = transform (inv r) r4
+        -- r6 = translate az (-i) r5
+        -- p2 = (!) p r6
+        -- steps = [[x], [p1], [r1, r2, r3], [m1, m2, m3], [l1, l2, l3], [u1], [l4, l5, l6], [m4, m5, m6], [r4, r5, r6], [p2]]
+        -- -------------------------------------------------------------------
         i = startIndex r - ringIndex r + n
         j = startIndex m - ringIndex m + turnoverCount 1 i r
         j' = startIndex m - ringIndex m + turnoverCount 2 i r
         k = startIndex l - ringIndex l + turnoverCount 0 j' m
         turnoverCount offset ind wheel =
-            sum [(ind - offset + (revIndex t)) `div` lettersCount - (if startIndex wheel > index t then 1 else 0) | t <- turnover wheel]
-        startIndex = index . startPosition
-        ringIndex = index . ringOffset
+            sum [(1 + ind - offset + revIndex az t) `div` length az - (if startIndex wheel > index az t then 1 else 0) | t <- turnover wheel]
+        startIndex = index az . startPosition
+        ringIndex = index az . ringOffset
 
 encrypt :: Key -> String -> String
-encrypt (Key (wheelL:wheelM:wheelR:_) (ringL:ringM:ringR:_) (startL:startM:startR:_) crossPlugging) =
-    encrypt' (Enigma p etw l m r ukw) 1
+encrypt (Key
+            (wheelL:wheelM:wheelR:_)
+            (ringL:ringM:ringR:_)
+            (startL:startM:startR:_)
+            crossPlugging) =
+    encrypt' (Enigma _AZ p etw l m r ukw) 1
     where
-        p = makePlugboard crossPlugging
+        p = makePlugboard _AZ crossPlugging
         etw = setWheel _ETW 'A' 'A'
         l = setWheel wheelL startL ringL
         m = setWheel wheelM startM ringM
         r = setWheel wheelR startR ringR
         ukw = setWheel _UKWB 'A' 'A'
-
-
---  encrypt (Key [_I, _II, _III] "AAA" "CFA" []) (take 100 $ repeat 'A')
---  SQPEUGJRCXZWPFYIYYBWLJEWROUVKPOZTCEUWTFJZQWPBQLOTTSRMDFLGXBXZRYQKDGJRZEZMDHJNQYPDJWCJFJLFNTRSNCNLGSK
--- test/TestEnigma.hs
+++ test/TestEnigma.hs
@@ -1,4 +1,193 @@
 module Main (main) where
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Enigma
+import Data.Map (fromList)
+
+referenceWiring = fromList [
+    ('A','E'),
+    ('B','B'),
+    ('C','A'),
+    ('D','C'),
+    ('E','D'),
+    ('F','F')
+    ]
+
+referenceWheel = Wheel {
+    wiring = referenceWiring,
+    notch = "C",
+    turnover = "B",
+    startPosition = 'E',
+    ringOffset = 'A'
+    }
+
+emptyPlugboard = makePlugboard "ABCD" []
+emptyWheel = makeWheel "ABCD" "ABCD" "" ""
+emptyWheelWithNotch = makeWheel "ABCD" "ABCD" "" "D"
+reverseWheel = makeWheel "ABCD" "DCBA" "" ""
+oneChangeWheel = makeWheel "ABCD" "ABDC" "" ""
+rightWheelEnigma = Enigma "ABCD" emptyPlugboard emptyWheel emptyWheel emptyWheel oneChangeWheel reverseWheel
+middleWheelEnigma = Enigma "ABCD" emptyPlugboard emptyWheel emptyWheel oneChangeWheel emptyWheelWithNotch reverseWheel
+leftWheelEnigma = Enigma "ABCD" emptyPlugboard emptyWheel oneChangeWheel emptyWheelWithNotch emptyWheelWithNotch reverseWheel
+
+indexTest :: Test
+indexTest = TestList [
+    TestCase $ assertEqual "" 0 (index ['A'..'F'] 'A'),
+    TestCase $ assertEqual "" 5 (index ['A'..'F'] 'F')
+    ]
+
+revIndexTest :: Test
+revIndexTest = TestList [
+    TestCase $ assertEqual "" 5 (revIndex ['A'..'F'] 'A'),
+    TestCase $ assertEqual "" 0 (revIndex ['A'..'F'] 'F')
+    ]
+
+makeWiringTest :: Test
+makeWiringTest = TestList [
+    TestCase $ assertEqual ""
+        referenceWiring
+        (makeWiring ['A'..'F'] "EBACDF")
+    ]
+
+makeWheelTest :: Test
+makeWheelTest = TestList [
+    TestCase $ assertEqual ""
+        referenceWheel
+        (makeWheel ['A'..'F'] "EBACDF" "C" "B")
+    ]
+
+makePlugboardTest :: Test
+makePlugboardTest = TestList [
+    TestCase $ assertEqual ""
+        (fromList [
+            ('A','F'),
+            ('B','B'),
+            ('C','D'),
+            ('D','C'),
+            ('E','E'),
+            ('F','A')]
+        )
+        (makePlugboard ['A'..'F'] ["CD", "FA"])
+    ]
+
+setWheelTest :: Test
+setWheelTest = TestList [
+    TestCase $ assertEqual "startPosition and ringOffset settable"
+        referenceWheel
+        (setWheel
+            (Wheel {
+                wiring = fromList [
+                    ('A','E'),
+                    ('B','B'),
+                    ('C','A'),
+                    ('D','C'),
+                    ('E','D'),
+                    ('F','F')
+                ],
+                notch = "C",
+                turnover = "B",
+                startPosition = 'C',
+                ringOffset = 'D'}
+            )
+            'E' 'A'
+        )
+    ]
+
+invTest :: Test
+invTest = TestList [
+    TestCase $ assertEqual ""
+        referenceWheel
+        (inv
+            (Wheel {
+                wiring = fromList [
+                    ('E','A'),
+                    ('B','B'),
+                    ('A','C'),
+                    ('C','D'),
+                    ('D','E'),
+                    ('F','F')
+                ],
+                notch = "C",
+                turnover = "B",
+                startPosition = 'E',
+                ringOffset = 'A'}
+            )
+        )
+    ]
+
+rotateTest :: Test
+rotateTest = TestList [
+    TestCase $ assertEqual "" "BCDEFA" (rotate 1 "ABCDEF")
+    ]
+
+translateTest :: Test
+translateTest = TestList [
+    TestCase $ assertEqual "" 'D' (translate ['A'..'F'] 1 'C'),
+    TestCase $ assertEqual "" 'A' (translate ['A'..'F'] 1 'F')
+    ]
+
+transformTest :: Test
+transformTest = TestList [
+    TestCase $ assertEqual ""
+        'D'
+        (transform (Wheel {
+            wiring = fromList [
+                ('A','E'),
+                ('B','B'),
+                ('C','A'),
+                ('D','C'),
+                ('E','D'),
+                ('F','F')
+            ],
+            notch = "C",
+            turnover = "B",
+            startPosition = 'E',
+            ringOffset = 'A'}
+        ) 'E')
+    ]
+
+encryptRightWheelTest :: Test
+encryptRightWheelTest = TestList [
+    TestCase $ assertEqual ""
+        "DCDCDCDCDCDC"
+        (encrypt' rightWheelEnigma 1 "AAAAAAAAAAAA")
+    ]
+
+encryptMiddleWheelTest :: Test
+encryptMiddleWheelTest = TestList [
+    TestCase $ assertEqual ""
+        "CCCDDDDCCCCDDDDCCCCDDDDCCCCDDDDCCCCDDDDCCCCDDDDCCC"
+        (encrypt' middleWheelEnigma 1 (replicate 50 'A'))
+    ]
+
+encryptLeftWheelTest :: Test
+encryptLeftWheelTest = TestList [
+    TestCase $ assertEqual ""
+        "CCCCCCCCCCCCDDDDDDDDDDDDDDDDCCCCCCCCCCCCCCCCDDDDDD"
+        (encrypt' leftWheelEnigma 1 (replicate 50 'A'))
+    ]
+
+enigmaM3Test :: Test
+enigmaM3Test = TestList [
+    TestCase $ assertEqual ""
+        "SQPEUGJRCXZWPFYIYYBWLOEWROUVKPOZTCEUWTFJZQWPBQLDTTSRMDFLGXBXZRYQKDGJRZEZMKHJNQYPDJWCJFJLFNTRSNCNLGSS"
+        (encrypt (Key [_I, _II, _III] "AAA" "CFA" []) (replicate 100 'A'))
+    ]
+
+main :: IO Counts
+main = runTestTT $ TestList [
+    indexTest,
+    revIndexTest,
+    makeWiringTest,
+    makeWheelTest,
+    makePlugboardTest,
+    setWheelTest,
+    invTest,
+    rotateTest,
+    translateTest,
+    transformTest,
+    encryptRightWheelTest,
+    encryptMiddleWheelTest,
+    encryptLeftWheelTest,
+    enigmaM3Test
+    ]
